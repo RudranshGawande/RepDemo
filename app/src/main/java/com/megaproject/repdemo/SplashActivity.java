265c0fd96e2d9f78490a6d71ac3ae0dfda9e653f
giris <girishkakde111@gmail.com>
2025-08-14 12:48:39 +0530

    Splash Page Updated

--- app/src/main/java/com/megaproject/repdemo/SplashActivity.java
+++ app/src/main/java/com/megaproject/repdemo/SplashActivity.java
@@ -13,7 +13,5 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-
     }
 }
