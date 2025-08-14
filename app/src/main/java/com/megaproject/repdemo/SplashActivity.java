cbdf0208fd5a6af0fe447e358835410fef710ad3
giris <girishkakde111@gmail.com>
2025-08-14 12:49:35 +0530

    Splash Page Updated

--- app/src/main/java/com/megaproject/repdemo/SplashActivity.java
+++ app/src/main/java/com/megaproject/repdemo/SplashActivity.java
@@ -13,5 +13,6 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 }
